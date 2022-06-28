158184d714f9dec7885f82508f0d36e2b27be653
Traxixzs <naitamarelyas@gmail.com>
2022-06-28 20:20:44 +0200

    version 1

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import time
+
+access_path = "VOTRE CHEMIN D'ACCES"
+list = os.listdir(access_path)
+
+for x in list:
+    date_creation = time.ctime(os.path.getmtime(access_path + x))
+    date_decouper = date_creation.split()
+    date_creation_obj = time.strptime(date_creation, '%a %b %d %H:%M:%S %Y')
+    os.makedirs(access_path + date_decouper[4], exist_ok=True)
+    os.makedirs(access_path + date_decouper[4] + '\\' + date_decouper[1], exist_ok=True)
+    source = access_path + x
+    destination = access_path + date_decouper[4] + '\\' + date_decouper[1]
+    shutil.move(source, destination)
